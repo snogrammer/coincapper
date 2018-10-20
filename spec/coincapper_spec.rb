7a8cdfe580ee0a10176c71ba4b763315c1cdb745
kurt-smith <13201143+kurt-smith@users.noreply.github.com>
2018-10-20 15:33:54 -0600

    Historical hotfix (#3)
    * 1.2.1 version bump
    
    * Casting String to Float with commas parses up to comma. Strip commas before casting to float

--- spec/coincapper_spec.rb
+++ spec/coincapper_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe CoinCapper do
-  it { expect(CoinCapper::VERSION).to eq('1.2.0') }
+  it { expect(CoinCapper::VERSION).to eq('1.2.1') }
 
   describe '#coins' do
     it 'returns all coins on default' do
@@ -131,8 +131,8 @@ describe CoinCapper do
         high: 1.05,
         low: 0.92497,
         close: 0.935619,
-        volume: 24.0,
-        market_cap: 626.0,
+        volume: 24_492_400.0,
+        market_cap: 626_839_000.0,
         average: 0.987485
       )
     end
